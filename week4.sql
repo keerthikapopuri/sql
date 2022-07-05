22e244a12cabc0e3f9100cd866f14ae337c17704
Popuri Keerthika <79918843+keerthikapopuri@users.noreply.github.com>
2022-07-05 14:49:36 +0530

    Update week4.sql

--- week4.sql
+++ week4.sql
@@ -87,6 +87,7 @@ MAX(SALARY) DEPT_NAME
       95000 Physics
 
 7 rows selected.
+-------------------------------------------9th question missing----------------------------------------------------------------------------
 SQL> select max(salary),dept_name from instructor group by dept_name;
 
 MAX(SALARY) DEPT_NAME
@@ -143,6 +144,7 @@ SQL> select count(distinct t.ID)
 COUNT(DISTINCTT.ID)
 -------------------
                   6
+ 2nd question missing------------------------------------------------------------------------------------------------------------------------------
 SQL> select course_id from teaches t1 where t1.semester='Fall' and t1.year=2009
   2  and exists (select * from teaches t2 where t1.course_id=t2.course_id and t2.semester='Spring' and t2.year=2010)
   3  ;
@@ -165,7 +167,7 @@ SQL> select t1.ID from takes t1 where not exists
   4  (select t2.course_id from takes t2 where t1.ID=t2.ID));
 
 no rows selected
-
+wrong-----------------------------------------------------------------------------------------------------
 SQL> select title from course
   2  where course_id in
   3  (select course_id from teaches where year=2009 group by course_id having count(course_id)<=1);
@@ -185,14 +187,21 @@ SQL> select title from course
 TITLE
 --------------------------------------------------
 Game Design
-SQL> select avg(salary)
-  2  from instructor
-  3  where dept_name in (select dept_name from instructor group by dept_name having avg(salary)>4200
-0);
 
-AVG(SALARY)
------------
-      78000
+SQL> select dept_name,avg_sal
+  2  from(select dept_name,avg(salary) avg_sal from instructor group by dept_name)
+  3  where avg_sal>42000;
+
+DEPT_NAME               AVG_SAL
+-------------------- ----------
+Biology                   72000
+Comp. Sci.           77333.3333
+Elec. Eng.                80000
+Finance                   85000
+History                   61000
+Physics                   91000
+
+6 rows selected.
 
 SQL> select dept_name 
   2  from department 
@@ -210,3 +219,31 @@ NAME
 Gold
 Califieri
 Singh
+SQL> select s.name,s.ID from student s,takes t where s.ID=t.ID(+) and not exists
+  2  ((select course_id from takes t2 where t.ID=t2.ID)
+  3  INTERSECT
+  4  (select course_id from section where semester='Fall' and year=2009));
+
+NAME                 ID
+-------------------- -----
+Brandt               19991
+Chavez               23121
+Sanchez              55739
+Snow                 70557
+Aoi                  76653
+Tanaka               98988
+Tanaka               98988
+SQL> select min(salary) from instructor
+  2  where salary in (select max(salary) from instructor group by dept_name);
+
+MIN(SALARY)
+-----------
+      40000
+      
+SQL> select i.name,i.ID from instructor i,teaches t,course c where not exists
+  2  ((select course_id from course where dept_name='Comp. Sci.')
+  3  MINUS
+  4  (select t2.course_id from teaches t2 where t.ID=t2.ID));
+
+no rows selected
+
